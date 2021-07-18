7aa6e08bb8ee6ffee09f6022217e4c9810fdd514
sykq <genericmailacc@gmail.com>
2021-07-18 11:14:52 +0200

    add KDoc to Command

--- twitch-chat-client-core/src/main/kotlin/io/github/sykq/tcc/bot/action/Command.kt
+++ twitch-chat-client-core/src/main/kotlin/io/github/sykq/tcc/bot/action/Command.kt
@@ -1,3 +1,9 @@
 package io.github.sykq.tcc.bot.action
 
+/**
+ * Defines a command with optional arguments from an incoming text message identified by the [OnCommandAction.command].
+ * @param command the command sent (e.g. `!help`).
+ * @param arguments optionally provided arguments (e.g. for the command `!help me now`, this would result in the list
+ * of `[me, now]` as arguments).
+ */
 data class Command(val command: String, val arguments: List<String>)
