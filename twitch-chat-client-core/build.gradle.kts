61e378ff43e38fb8d24f319705b30be4b7f67a0c
sykq <genericmailacc@gmail.com>
2021-07-30 21:49:24 +0200

    add reactor-extra for Retry

--- twitch-chat-client-core/build.gradle.kts
+++ twitch-chat-client-core/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     api("org.springframework:spring-webflux")
     api("io.projectreactor.netty:reactor-netty-http")
     api("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    api("io.projectreactor.addons:reactor-extra")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$coroutinesVersion")
 
